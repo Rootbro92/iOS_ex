b2b0851d8eeede6cf23aa2951d8964317bc886b4
Parkgeunhyung <rootbro@kakao.com>
2020-02-28 22:34:39 +0900

    Multi Unwind

---
+++ Scene-Trans/Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 박근형 on 2020/02/28.
+//  Copyright © 2020 pgh. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Scene-Trans/Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 박근형 on 2020/02/28.
+//  Copyright © 2020 pgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
